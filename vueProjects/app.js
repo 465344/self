4d77f2fa068c78958f5776156e6b51059e12a298
aojingtao <649644778@qq.com>
2019-02-21 14:48:44 +0800

    vue--学习

---
+++ vueProjects/app.js
@@ -0,0 +1,49 @@
+//实例化vue对象
+new Vue({
+    el:"#vue-app",
+    data:{
+        name:"xiaoming",
+        job:"web开发",
+        website:"http://www.baidu.com",
+        websiteTag:"<a href='http://www.baidu.com'>Thenewstet</a>"
+    },
+    methods:{
+        greet:function (time) {
+            console.log("hellow word"+time+""+this.name+ "!")
+        }
+    }
+});
+new Vue({
+    el:"#vue-app1",
+    data:{
+        age:30,
+        x:0,
+        y:0
+    },
+    methods:{
+        add:function (inc) {
+            this.age += inc;
+        },
+        subtract:function (dec) {
+            this.age -= dec;
+        },
+        updateXY:function (event) {
+            // console.log(event)
+            this.x = event.offsetX;
+            this.y = event.offsetY
+        },
+        stopMoving:function (event) {
+            // event.stopPropagation();
+        },
+        alert:function () {
+            alert("djklsj")
+        }
+    }
+
+});
+/*
+* el:element 需要获取的元素，一定是html中的根容器元素.
+* data:是用于数据的存储
+* methods:用于存储各种方法
+* data-binding:给属性绑定对应的值
+* */
