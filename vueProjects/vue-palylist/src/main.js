1e3e82eefa4067b8fe3c6f8b00273a2a758092cd
aojingtao <649644778@qq.com>
2019-03-01 18:53:59 +0800

    vue--组件

--- vueProjects/vue-palylist/src/main.js
+++ vueProjects/vue-palylist/src/main.js
@@ -2,12 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// import Users from './components/users'
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+//全局注册组建
+// Vue.component("users",Users)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
-})
+});
+//index.html ->main.js->App.vue
